6eb43e40706c37728d092ae9dd24bcd9d8bbcf75
SadMeercat <igor9823@gmail.com>
2022-10-21 21:28:36 +0300

    Solved test task

---
+++ Test_task/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Test_task
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.calcBtn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.xTextBox = new System.Windows.Forms.TextBox();
+            this.yTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label3 = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(428, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(310, 310);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // calcBtn
+            // 
+            this.calcBtn.Location = new System.Drawing.Point(428, 328);
+            this.calcBtn.Name = "calcBtn";
+            this.calcBtn.Size = new System.Drawing.Size(146, 23);
+            this.calcBtn.TabIndex = 1;
+            this.calcBtn.Text = "Посчитать площадь";
+            this.calcBtn.UseVisualStyleBackColor = true;
+            this.calcBtn.Click += new System.EventHandler(this.calcBtn_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(243, 446);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // xTextBox
+            // 
+            this.xTextBox.Location = new System.Drawing.Point(283, 12);
+            this.xTextBox.Name = "xTextBox";
+            this.xTextBox.Size = new System.Drawing.Size(100, 20);
+            this.xTextBox.TabIndex = 3;
+            // 
+            // yTextBox
+            // 
+            this.yTextBox.Location = new System.Drawing.Point(283, 38);
+            this.yTextBox.Name = "yTextBox";
+            this.yTextBox.Size = new System.Drawing.Size(100, 20);
+            this.yTextBox.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(263, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(263, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Y";
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(283, 64);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(100, 22);
+            this.addBtn.TabIndex = 7;
+            this.addBtn.Text = "Добавить";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.Location = new System.Drawing.Point(283, 92);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(100, 22);
+            this.deleteBtn.TabIndex = 8;
+            this.deleteBtn.Text = "Удалить";
+            this.deleteBtn.UseVisualStyleBackColor = true;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "X";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Y";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(378, 368);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Ответ:";
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(424, 368);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(0, 13);
+            this.resultLabel.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(750, 470);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.yTextBox);
+            this.Controls.Add(this.xTextBox);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.calcBtn);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button calcBtn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox xTextBox;
+        private System.Windows.Forms.TextBox yTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.Button deleteBtn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label resultLabel;
+    }
+}
+
---
+++ Test_task/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Test_task
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void DrowPic()//Drows a shape
+        {
+            Pen myPen = new Pen(Color.Black, 2f);
+            Graphics graphics = pictureBox1.CreateGraphics();
+            graphics.Clear(Color.White);
+            PointF[] points = new PointF[dataGridView1.RowCount+1];
+            float[] picBoxCenter = new float[2] { pictureBox1.Width / 2, pictureBox1.Height / 2 };//find center picture box
+
+            float scale = 10;
+
+            for (int i = 0; i < dataGridView1.RowCount; i++)//forms points in array
+            {
+                float xPoint = picBoxCenter[0] + (float)dataGridView1[0, i].Value * scale;
+                float yPoint = picBoxCenter[1] - (float)dataGridView1[1, i].Value * scale;
+                points[i] = new PointF(xPoint, yPoint);
+            }
+
+            if (dataGridView1.RowCount > 1)//drows
+            {
+                points[points.Length-1] = points[0];
+                graphics.DrawLines(myPen, points);
+            }
+        }
+        private void addBtn_Click(object sender, EventArgs e)//add one point from text boxes
+        {
+            xTextBox.Text.Replace(".", ",");
+            yTextBox.Text.Replace(".", ",");
+            try
+            {
+                float x = float.Parse(xTextBox.Text);
+                float y = float.Parse(yTextBox.Text);
+                dataGridView1.Rows.Add();
+                dataGridView1[0, dataGridView1.RowCount-1].Value = x;
+                dataGridView1[1, dataGridView1.RowCount-1].Value = y;
+                xTextBox.Text = "";
+                yTextBox.Text = "";
+                xTextBox.Focus();
+                DrowPic();
+            }
+            catch(FormatException)
+            {
+                MessageBox.Show("Неправильный ввод x или y");
+            }
+        }
+
+        private void deleteBtn_Click(object sender, EventArgs e)//delete selected row
+        {
+            dataGridView1.Rows.Remove(dataGridView1.Rows[dataGridView1.CurrentRow.Index]);
+            if (dataGridView1.RowCount > 0)
+            {
+                DrowPic();
+            }
+        }
+
+        private void calcBtn_Click(object sender, EventArgs e)//solve task
+        {
+            try
+            {
+                if (dataGridView1.RowCount == 0)
+                {
+                    throw new Exception("Кол-во точек не может быть равно 0");
+                }
+                float[][] myPoints = new float[dataGridView1.RowCount][];
+
+                float summ1 = 0, summ2 = 0;
+                for (int i = 0; i < dataGridView1.RowCount - 1; i++)
+                {
+                    summ1 += (float)dataGridView1[0, i].Value * (float)dataGridView1[1, i + 1].Value;
+                    summ2 += (float)dataGridView1[1, i].Value * (float)dataGridView1[0, i + 1].Value;
+                }
+                summ1 += (float)dataGridView1[0, dataGridView1.RowCount - 1].Value * (float)dataGridView1[1, 0].Value;
+                summ2 += (float)dataGridView1[1, dataGridView1.RowCount - 1].Value * (float)dataGridView1[0, 0].Value;
+                float result = (summ1 - summ2) / 2;
+                resultLabel.Text = Convert.ToString(result);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
